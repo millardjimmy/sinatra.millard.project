657106e638bd447718f805a20358f04e835b81fc
millardjimmy <millardjimmy@gmail.com>
2020-06-29 17:01:35 -0600

    Params are now more managable

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -2,7 +2,7 @@ class Incident < ActiveRecord::Base
     belongs_to :user
 
     def self.valid_params?(params)
-      params[:incident][:crime_category] != "" && params[:incident][:agency_responsible] != "" && params[:incident][:incident_date] != ""
+      params[:incident][:crime_category] != "" #&& params[:incident][:agency_responsible] != "" && params[:incident][:incident_date] != ""
     end
     
     
