cef9351b4deb0a4a0869ac6a1857ac8c01bb1b7d
millardjimmy <millardjimmy@gmail.com>
2020-07-15 18:55:17 -0600

    helpers now in app controller

--- app/helpers/helpers.rb
+++
@@ -1,9 +0,0 @@
-# class Helpers
-#     def self.is_logged_in?(session)
-#       !!session[:user_id]
-#     end
-  
-#     def self.current_user(session)
-#       User.find_by(id: session[:user_id])
-#     end
-#   end
